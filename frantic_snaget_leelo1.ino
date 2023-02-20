5de130964b9d0d9b2fdb05c2b2923c3d2e7487f7
Yohjit Chopra <103960257+Yohjit-2910@users.noreply.github.com>
2023-02-21 01:34:22 +0530

    Add files via upload

---
+++ frantic_snaget_leelo1.ino
@@ -0,0 +1,26 @@
+int SensorPIR = 12;
+int value = 0;
+int LED = 2;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+  pinMode(SensorPIR, INPUT);
+}
+
+void loop()
+{
+  value = digitalRead(SensorPIR);
+  if (value)
+  {
+    digitalWrite(LED, 1);
+    Serial.println("Motion Detected");
+  }
+  else
+  {
+    digitalWrite(LED, 0);
+    Serial.println("NO Motion Detected");
+  }
+  delay(1000);
+}
